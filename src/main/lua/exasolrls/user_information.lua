0e5e1a30c2639830676ce1b733e506b97ec5da30
Sebastian Bär <sebastian.baer@exasol.com>
2020-09-16 13:07:10 +0200

    #7: Group security complete.

--- src/main/lua/exasolrls/user_information.lua
+++ src/main/lua/exasolrls/user_information.lua
@@ -11,10 +11,8 @@ local M = {}
 --
 function M.get_groups(source_schema_id)
     local groups = {}
-    --local sql = 'SELECT "EXA_RLS_GROUP" FROM "' .. source_schema_id .. '"."EXA_GROUP_MEMBERS" WHERE "EXA_RLS_USER" = CURRENT_USER'
-    local sql = 'SELECT "EXA_RLS_GROUP" FROM VALUES (\'G1\')'
-    --local sql = 'SELECT \'G1\' FROM "EXA_GROUP_MEMBERS"'
-    log.trace("Determining user groups with: %s", sql)
+    local sql = 'SELECT "EXA_RLS_GROUP" FROM "' .. source_schema_id
+        .. '"."EXA_GROUP_MEMBERS" WHERE "EXA_RLS_USER" = CURRENT_USER'
     local ok, result = exa.pquery(sql)
     if(ok) then
         for i = 1, #result do
@@ -22,7 +20,7 @@ function M.get_groups(source_schema_id)
         end
         log.debug("Current user is a member of %d RLS groups.", #groups)
     else
-        log.warn("W-USI-1: Unable to determine group membership of current user. Cause: %s", result)
+        log.warn("W-RLS-USI-1: Unable to determine group membership of current user. Cause: %s", result)
     end
     return groups
 end
