7c6468280774743e27327fdac250106d236ee6ad
Sebastian Bär <sebastian.baer@exasol.com>
2021-05-27 09:51:08 +0200

    #37: Block combination of group and role security (#55)

--- src/test/lua/exasolrls/test_query_rewriter.lua
+++ src/test/lua/exasolrls/test_query_rewriter.lua
@@ -1,30 +1,14 @@
 local luaunit = require("luaunit")
-local mockagne = require("mockagne")
-local log = require("remotelog")
-log.set_level("TRACE")
-
-local when, any = mockagne.when, mockagne.any
+local rewriter = require("exasolrls.query_rewriter")
 
 _G.test_query_rewriter = {}
 
-function test_query_rewriter:setUp()
-    self.user = mockagne.getMock()
-    package.preload["exasolrls.user_information"] = function() return self.user end
-    self.rewriter = require("exasolrls.query_rewriter")
-end
-
-function test_query_rewriter.tearDown()
-    package.loaded["exasolrls.user_information"] = nil
-    package.loaded["exasolrls.query_rewriter"] = nil
-end
-
-function test_query_rewriter:assert_rewrite(original_query, source_schema, adapter_cache, expected)
-    local rewritten_query = self.rewriter.rewrite(original_query, source_schema, adapter_cache)
+local function assert_rewrite(original_query, source_schema, adapter_cache, expected)
+    local rewritten_query = rewriter.rewrite(original_query, source_schema, adapter_cache)
     luaunit.assertEquals(rewritten_query, expected)
 end
 
-function test_query_rewriter:test_unprotected_table()
-    when(self.user.get_groups(any())).thenAnswer({})
+function test_query_rewriter.test_unprotected_table()
     local original_query = {
         type = "select",
         selectList = {
@@ -33,11 +17,10 @@ function test_query_rewriter:test_unprotected_table()
         },
         from = {type  = "table", name = "UNPROT"}
     }
-    self:assert_rewrite(original_query, "S", "UNPROT:---", 'SELECT "UNPROT"."C1", "UNPROT"."C2" FROM "S"."UNPROT"')
+    assert_rewrite(original_query, "S", "UNPROT:---", 'SELECT "UNPROT"."C1", "UNPROT"."C2" FROM "S"."UNPROT"')
 end
 
-function test_query_rewriter:test_tenant_protected_table()
-    when(self.user.get_groups(any())).thenAnswer({})
+function test_query_rewriter.test_tenant_protected_table()
     local original_query = {
         type = "select",
         selectList = {
@@ -45,12 +28,11 @@ function test_query_rewriter:test_tenant_protected_table()
         },
         from = {type  = "table", name = "PROT"}
     }
-    self:assert_rewrite(original_query, "S", "PROT:t--",
+    assert_rewrite(original_query, "S", "PROT:t--",
         'SELECT "PROT"."C1" FROM "S"."PROT" WHERE ("PROT"."EXA_ROW_TENANT" = CURRENT_USER)')
 end
 
-function test_query_rewriter:test_group_protected_table()
-    when(self.user.get_groups(any())).thenAnswer({"G1", "G2"})
+function test_query_rewriter.test_group_protected_table()
     local original_query = {
         type = "select",
         selectList = {
@@ -58,14 +40,13 @@ function test_query_rewriter:test_group_protected_table()
         },
         from = {type  = "table", name = "PROT"}
     }
-    self:assert_rewrite(original_query, "S", "PROT:-g-",
+    assert_rewrite(original_query, "S", "PROT:-g-",
         'SELECT "PROT"."C1" FROM "S"."PROT" WHERE EXISTS('
         .. 'SELECT 1 FROM "S"."EXA_GROUP_MEMBERS" WHERE (("EXA_GROUP_MEMBERS"."EXA_GROUP" = "PROT"."EXA_ROW_GROUP")'
         .. ' AND ("EXA_GROUP_MEMBERS"."EXA_USER_NAME" = CURRENT_USER)))')
 end
 
-function test_query_rewriter:test_tenant_plus_group_protected_table()
-    when(self.user.get_groups(any())).thenAnswer({"G1", "G2"})
+function test_query_rewriter.test_tenant_plus_group_protected_table()
     local original_query = {
         type = "select",
         selectList = {
@@ -73,14 +54,14 @@ function test_query_rewriter:test_tenant_plus_group_protected_table()
         },
         from = {type  = "table", name = "PROT"}
     }
-    self:assert_rewrite(original_query, "S", "PROT:tg-",
+    assert_rewrite(original_query, "S", "PROT:tg-",
         'SELECT "PROT"."C1" FROM "S"."PROT" WHERE (("PROT"."EXA_ROW_TENANT" = CURRENT_USER) '
         .. 'OR EXISTS('
         .. 'SELECT 1 FROM "S"."EXA_GROUP_MEMBERS" WHERE (("EXA_GROUP_MEMBERS"."EXA_GROUP" = "PROT"."EXA_ROW_GROUP")'
         .. ' AND ("EXA_GROUP_MEMBERS"."EXA_USER_NAME" = CURRENT_USER))))')
 end
 
-function test_query_rewriter:test_role_protected_table()
+function test_query_rewriter.test_role_protected_table()
     local original_query = {
         type = "select",
         selectList = {
@@ -88,7 +69,7 @@ function test_query_rewriter:test_role_protected_table()
         },
         from = {type  = "table", name = "PROT"}
     }
-    self:assert_rewrite(original_query, "S", "PROT:--r",
+    assert_rewrite(original_query, "S", "PROT:--r",
         'SELECT "PROT"."C1" FROM "S"."PROT" WHERE (BIT_CHECK("PROT"."EXA_ROW_ROLES", 63)'
         .. ' OR EXISTS('
         .. 'SELECT 1 FROM "S"."EXA_RLS_USERS"'
@@ -96,8 +77,7 @@ function test_query_rewriter:test_role_protected_table()
         .. ' AND (BIT_AND("PROT"."EXA_ROW_ROLES", "EXA_RLS_USERS"."EXA_ROLE_MASK") <> 0))))')
 end
 
-function test_query_rewriter:test_tenant_plus_role_protected_table()
-    when(self.user.get_role_mask(any())).thenAnswer(13)
+function test_query_rewriter.test_tenant_plus_role_protected_table()
     local original_query = {
         type = "select",
         selectList = {
@@ -105,13 +85,27 @@ function test_query_rewriter:test_tenant_plus_role_protected_table()
         },
         from = {type  = "table", name = "PROT"}
     }
-    self:assert_rewrite(original_query, "S", "PROT:t-r",
+    assert_rewrite(original_query, "S", "PROT:t-r",
         'SELECT "PROT"."C1" FROM "S"."PROT" WHERE (("PROT"."EXA_ROW_TENANT" = CURRENT_USER)'
             ..' OR BIT_CHECK("PROT"."EXA_ROW_ROLES", 63)'
             ..' OR EXISTS('
         .. 'SELECT 1 FROM "S"."EXA_RLS_USERS"'
         .. ' WHERE (("EXA_RLS_USERS"."EXA_USER_NAME" = CURRENT_USER)'
         .. ' AND (BIT_AND("PROT"."EXA_ROW_ROLES", "EXA_RLS_USERS"."EXA_ROLE_MASK") <> 0))))')
+end
+
+function test_query_rewriter.test_combination_of_group_and_role_security_throws_error()
+    for _, protection in ipairs({"-gr", "tgr"}) do
+        local original_query = {
+            type = "select",
+            selectList = {
+                {type = "column", name = "C1", tableName = "PROT"},
+            },
+            from = {type  = "table", name = "PROT"}
+        }
+        luaunit.assertErrorMsgContains("Combination of role and group security not supported",
+            rewriter.rewrite, original_query, "PROT", "PROT:" .. protection)
+    end
 end
 
 os.exit(luaunit.LuaUnit.run())
