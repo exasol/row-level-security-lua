1f49aee0ad6dde1fec5ed7625f92b0d15991649c
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-03-27 10:59:36 +0200

    Remove decommissioned maven.exasol.com Fixes #127

--- tools/runtests.sh
+++ tools/runtests.sh
@@ -2,13 +2,9 @@
 
 # This script finds and runs Lua unit tests, collects coverage and runs static code analysis.
 
-readonly script_dir=$(dirname "$(readlink -f "$0")")
-if [[ -v $1 ]]
-then
-    readonly base_dir="$1"
-else
-    readonly base_dir=$(readlink -f "$script_dir/..")
-fi
+readonly script_dir=/Users/chp/git/row-level-security-lua/tools
+
+    readonly base_dir=/Users/chp/git/row-level-security-lua/
 
 readonly exit_ok=0
 readonly exit_software=2
